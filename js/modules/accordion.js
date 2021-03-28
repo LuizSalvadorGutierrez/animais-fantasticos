7ffdf103d2a9f8ca8aadf6ef345afb1859d47bc5
LuizSalvadorGutierrez <LuizSalvadorGutierrez>
2021-03-28 17:37:01 -0300

    adicionado package-pack-json

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,11 +1,12 @@
 export default function initAccordion() {
   const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
   const activeClass = 'ativo';
-  
+
   function activeAccordion() {
     this.classList.toggle(activeClass);
     this.nextElementSibling.classList.toggle(activeClass);
   }
+
   if (accordionList.length) {
     accordionList[0].classList.add(activeClass);
     accordionList[0].nextElementSibling.classList.add(activeClass);
@@ -15,3 +16,4 @@ export default function initAccordion() {
     });
   }
 }
+console.log('teste')
--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,13 +1,13 @@
 export default function initAnimaNumeros() {
   function animaNumeros() {
-    const numeros = document.querySelectorAll("[data-numero]");
+    const numeros = document.querySelectorAll('[data-numero]');
 
     numeros.forEach((numero) => {
       const total = +numero.innerText;
-      const incremento = Math.floor(total / 100);
+      const inscremento = Math.floor(total / 100);
       let start = 0;
       const timer = setInterval(() => {
-        start += incremento;
+        start += inscremento;
         numero.innerText = start;
         if (start > total) {
           numero.innerText = total;
@@ -16,15 +16,16 @@ export default function initAnimaNumeros() {
       }, 25 * Math.random());
     });
   }
-  let observer
+
+  let observer;
   function handleMutation(mutation) {
-    if(mutation[0].target.classList.contains("ativo"));
+    if (mutation[0].target.classList.contains('ativo')) {
       observer.disconnect();
       animaNumeros();
+    }
   }
-
   observer = new MutationObserver(handleMutation);
 
-  const observerTarget = document.querySelector(".numeros");
+  const observerTarget = document.querySelector('.numeros');
   observer.observe(observerTarget, { attributes: true });
 }
